315ee9cdf9649acc32e724eb1fd796271815940d
1988-klee <calafaro@gmail.com>
2020-04-14 20:01:34 -0400

    subir proyecto

---
+++ oveja/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oveja
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            i++;
+            label3.Text = Convert.ToString(i);
+            MessageBox.Show("Aqui estoy..");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            i = 0;
+            label3.Text = Convert.ToString(i);
+            MessageBox.Show("Estoy Zzz");
+            Close();
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
